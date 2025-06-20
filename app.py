3c2d32c418df03d960627be6ef05c0092376d33b
Julio Ochoa <jochoam8@est.ups.edu.ec.com>
2025-06-20 12:59:24 -0500

    Tercer commit del proyecto codigo refactorizado app

--- app.py
+++ app.py
@@ -2,10 +2,9 @@ from librodiario import LibroDiario
 
 libro = LibroDiario()
 
-libro.agregar_transaccion('18/06/2025', 'Compra de laptop', 780, 'egreso')
-
-#libro.agregar_transaccion('18/06/2025', 'Venta de sensor TK-110', 780, 'Ingreso')
-
-libro.agregar_transaccion('18/06/2025', 'Compra de insumos de oficina', -85.60, 'egreso')
-
-print(libro.calcular_resumen())
+try:
+    libro.cargar_transacciones_desde_archivo("transacciones.csv")
+    libro.agregar_transaccion("18/06/2025", "Pago consultoría", 200.00, "ingreso")
+    libro.exportar_resumen("resumen_contable.txt")
+except Exception as e:
+    print(f"Error general: {e}")
